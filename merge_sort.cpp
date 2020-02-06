af752de8ce42dcacdb5a49794bf217359ecb37d2
Avinash Honkan <>
2020-02-06 02:43:10 -0500

    updating with merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void print_array(int *array, int len)
+{
+  for (int idx = 0; idx < len; idx ++)
+  {
+    printf("%d ", array[idx]);
+  }
+  printf("\n");
+}
 
 template<typename T>
 bool compare(T left, T right)
@@ -11,15 +19,22 @@ bool compare(T left, T right)
 }
 
 template<typename T>
-void merge_sort_kernel(T *left, size_t left_len, 
+void merge(T *left, size_t left_len, 
 					   T *right, size_t right_len, 
 					   T *out)
 {
 	size_t left_idx = 0;
 	size_t right_idx = 0;
 
-	while(true)
+  printf("Merge: L: %llx %ld  R: %llx %ld OUT: %llx\n",
+      (unsigned long long)left, left_len,
+      (unsigned long long)right, right_len,
+      (unsigned long long)out);
+      
+
+	while((left_idx < left_len) && (right_idx < right_len))
 	{
+    printf("Merge Compare L:%ld R:%ld\n", left_idx, right_idx);
 		if (compare(left[left_idx], right[right_idx]))
 		{
 			*out = left[left_idx];
@@ -32,45 +47,65 @@ void merge_sort_kernel(T *left, size_t left_len,
 			out ++;
 			right_idx ++;
 		}
+  }
 	
-		if (left_idx >= left_len)
+  if (left_idx >= left_len)
+	{
+		for (int i = right_idx; i < right_len; i++)
 		{
-			for (int i = right_idx; i < right_len; i++)
-			{
-				*out = right[i];
-				out ++;
-			}
-		}	
-		else if (right_idx >= right_len)
+      printf("Flush Right L:%ld R:%d\n", left_idx, i);
+			*out = right[i];
+			out ++;
+		}
+	}	
+	else if (right_idx >= right_len)
+	{
+		for (int i = left_idx; i < left_len; i++)
 		{
-			for (int i = left_idx; i < left_len; i++)
-			{
-				*out = left[i];
-				out ++;
-			}
-		}	
-
-	}
-
+      printf("Flush Left L:%d R:%ld\n", i, right_idx);
+			*out = left[i];
+			out ++;
+		}
+	}	
+  
 }
 
 template<typename T>
-void merge_sort(T *in_array, size_t len, T *out_array, size_t *out_array_idx)
+void merge_sort(T *in_array, size_t len, T *out_array)
 {
+  static int level = 0;
+
+  printf("%d in: %llx out: %llx len: %ld\n",
+            level, 
+            (unsigned long long)in_array, 
+            (unsigned long long)out_array,
+            len); 
+  level ++;
 
 	if (len == 0)
 		return;
 	else if (len == 1)
 	{
 		*out_array = *in_array;
-		*out_array_idx ++;
+		out_array ++;
 	}
 	else if (len == 2)
 	{
-		merge_sort_kernel(in_array, 1, 
-					      in_array + 1, 1, 
-					   	  out_array + out_array_idx);
-		*out_array_idx += 2;
+	  if (compare(in_array[0], in_array[1]))
+		{
+			*out_array = in_array[0];
+			out_array ++;
+			*out_array = in_array[1];
+			out_array ++;
+		}
+		else
+		{
+			*out_array = in_array[1];
+			out_array ++;
+			*out_array = in_array[0];
+			out_array ++;
+		}
+    
 	}
 	else
 	{
@@ -80,21 +115,49 @@ void merge_sort(T *in_array, size_t len, T *out_array, size_t *out_array_idx)
 		int right_len = len - left_len;
 
 		// Left sort
-		merge_sort(left, left_len, out_array, out_array_idx);
+		merge_sort(left, left_len, out_array);
+    printf("%d: left\n", level);
+    print_array(left, left_len);
 
 		// Right sort
-		merge_sort(right, right_len, out_array, out_array_idx);
+		merge_sort(right, right_len, out_array + left_len);
+    printf("%d: right\n", level);
+    print_array(right, right_len);
 
-
+/*
+    merge(out_array, left_len, 
+					out_array + left_len, right_len, 
+					in_array);
+*/
+    merge(left, left_len, 
+					right, right_len, 
+					out_array);
+    
+    printf("merged\n");
+    print_array(in_array, len);
 	}
 
+  level --;
 
 }
 
 
-
 int main(void)
 {
-	printf("hello wrld\n");
+//  int left[] = {0,3,6,10,15,21,28,36,41}; 
+  int left[] = {0,4,5,6,11,15,18,20}; 
+  int right[] = {1,2,3,7,8,9,19};
+  int in[] = {2,1,3,9,5,4};
+  int out[100] = {0};
 
+  size_t left_len = sizeof(left)/sizeof(left[0]);
+  size_t right_len = sizeof(right)/sizeof(right[0]);
+  size_t in_len = sizeof(in)/sizeof(in[0]);
+
+//  merge<int>(left, left_len, right, right_len, out);
+
+  merge_sort<int>(in, in_len, out);
+  
+  printf("sorted\n");
+  print_array(in, in_len);
 }
